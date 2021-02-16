145b2d70dc2a85265c387dd03eb8be45511b9b28
Pascal Gehring (Desktop) <pascal.gehring@gmx.ch>
2021-02-16 15:24:34 +0100

    9. Remote Data source

---
+++ lib/core/util/input_converter.dart
@@ -0,0 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:tdd/core/error/failures.dart';
+
+class InputConverter {
+  Either<Failure, int> stringToUnsignedInteger(String str) {
+    // TODO:
+  }
+}
+
+class InvalidInputFailure extends Failure {}
